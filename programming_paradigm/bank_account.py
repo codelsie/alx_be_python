af928d9ed9f04ec262bcdd2cf26e6b59ef7c166e
codelsie <lizzyogb1@gmail.com>
2025-07-14 15:07:32 +0100

    programming_paradigm

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,17 @@
+# bank_account.py
+
+class BankAccount:
+    def __init__(self, initial_balance=0):
+        self.__account_balance = initial_balance  # Private balance
+
+    def deposit(self, amount):
+        self.__account_balance += amount  # Add money
+
+    def withdraw(self, amount):
+        if self.__account_balance >= amount:
+            self.__account_balance -= amount  # Remove money
+            return True
+        return False  # Not enough money
+
+    def display_balance(self):
+        print(f"Current Balance: ${self.__account_balance}")  # Show balance
